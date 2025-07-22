42a0a9bf5c4c22dbeefcff3977e8e1342b400742
Kevin Kwong <a1176079417@gmail.com>
2025-07-22 17:06:33 +0800

    feat: 完善审核信息显示

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,11 +1,17 @@
 export interface Legal {
   id: number
   org_num: number
-  category: number
+  category: 0 | 1 // 0 corp  1 personal
   name: string
+
   identify: string
-  identify_card_front: string
-  identify_card_reverse: string
+
+  business_license?: string
+
+  // 身份证
+  identify_card_front?: string
+  identify_card_reverse?: string
+
   state: number
   updated: string // 2025-07-15 15:28:52
 }
