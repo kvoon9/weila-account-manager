195102d1031b4c6d54a6150486633dcdc6ce3cb4
kvoon9 <a1176079417@qq.com>
2025-07-29 17:40:18 +0800

    feat: 更新 network 包，采用 hookable api

--- app/composables/useWeilaApi.ts
+++ app/composables/useWeilaApi.ts
@@ -5,34 +5,45 @@ export function useWeilaApi() {
   const weilaApi = useState('weilaApi', () => new WeilaApi(
     '102036',
     'b3c658bd2e637c65efb134fb381d4a18',
-    {
-      baseURL: 'v2',
-      onStart() {
-        start()
-      },
-      onDone() {
-        finish()
-      },
-      onError(error: any) {
-        if (error instanceof Error) {
-          Message.error(error.message)
-        }
-        else {
-          const { errcode, errmsg } = error
-          if (errcode !== undefined && errmsg !== undefined) {
-            Message.error(`${errcode}: ${errmsg}`)
-          }
-          else {
-            console.error(error)
-          }
-        }
-      },
-      onAuthError() {
-        localStorage.setItem('token', '')
-        navigateTo('/login')
-      },
-    },
+    { baseURL: 'v2' },
   ))
+
+  weilaApi.value.hook('request:start', () => {
+    start()
+  })
+
+  weilaApi.value.hook('done', () => {
+    finish()
+  })
+
+  function onError(error: any) {
+    console.error('error', error)
+    if (error instanceof Error) {
+      Message.error(error.message)
+    }
+    else {
+      const { errcode, errmsg } = error
+      if (errcode !== undefined && errmsg !== undefined) {
+        Message.error(`${errcode}: ${errmsg}`)
+      }
+      else {
+        console.error(error)
+      }
+    }
+  }
+
+  weilaApi.value.hook('request:error', onError)
+  weilaApi.value.hook('response:error', onError)
+  weilaApi.value.hook('success', (data) => {
+    const isDebug = false
+    if (isDebug)
+      // eslint-disable-next-line no-console
+      console.log('data', data)
+  })
+  weilaApi.value.hook('auth:error', () => {
+    localStorage.setItem('token', '')
+    navigateTo('/login')
+  })
 
   return weilaApi
 }
